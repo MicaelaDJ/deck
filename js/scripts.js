79da24245d7593aad006cb39a3289a6e24116633
jasmine lee and micaela jawor <jasmine@>
2019-01-22 14:54:30 -0800

    add list to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
-var cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
-var suits =['Spades', 'Hearts', 'Diamonds', 'Clubs'];
-var deck =[];
-// var suit2 =['Hearts']
-// var suit3 =['Diamonds']
-// var suit4 =['Clubs']
-suits.forEach(function(suit) {
-  cards.forEach(function(card) {
-    deck.push(card + ' of ' + suit);
-  })
+$(document).ready(function() {
+
+  var cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+  var suits =['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+
+  suits.forEach(function(suit) {
+    cards.forEach(function(card) {
+      $("ul#deck").append("<li>" + card + ' of ' + suit + "</li>");
+    });
+  });
 });
-alert(deck);
